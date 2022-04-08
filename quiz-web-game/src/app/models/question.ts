522566d055439c3796f9e0c6c22c4221ae3007a8
tawfeek <tawfeeksami@gmail.com>
2022-04-08 23:26:45 +0300

    adding unescapeHtml to unescape special characters

--- quiz-web-game/src/app/models/question.ts
+++ quiz-web-game/src/app/models/question.ts
@@ -10,15 +10,16 @@ export class Question {
         public correct_answer: string,
         public incorrect_answers: string[]
     ) {
+        this.question = this.unescapeHtml(this.question);
         this.answersToDisplay();
      }
 
     answersToDisplay() {
         this.tmpArr = [];
         this.incorrect_answers.forEach(item => {
-            this.tmpArr.push(item);
+            this.tmpArr.push(this.unescapeHtml(item));
         });
-        this.tmpArr.push(this.correct_answer);
+        this.tmpArr.push(this.unescapeHtml(this.correct_answer));
         this.answerslist = this.randomArrayShuffle(this.tmpArr)
 
     }
@@ -34,4 +35,15 @@ export class Question {
         }
         return array;
     }
+
+    unescapeHtml(str) {
+        return str.replace(/&amp;/g, '&')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&quot;/g, '"')
+            .replace(/&#039;/g, "'")
+            .replace(/&deg;/g,"°")
+            .replace(/&Auml;/g,"Ä")
+            .replace(/&eacute;/g,"é");
+      }
 }
